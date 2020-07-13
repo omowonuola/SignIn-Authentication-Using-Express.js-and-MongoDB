61443dfcc01de53b0a3f820cec8ee53fb51d9fee
Omowonuola <oyedmola@gmail.com>
2020-07-13 02:09:05 +0100

    updated routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const { register, login, dashboard, users } = require('../controllers/auth');
 
+// Include other resource routers
+const bookingRouter = require('./bookings');
+
 const router = express.Router();
 
+// Re-route into other resource routers
+// router.use('/:userId/bookings', bookingRouter);
+
 const { protect } = require('../middleware/auth');
+
 
 router.post('/register', register);
 router.post('/login', login);
